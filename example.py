06adaee1b45ab91b01b12feef87016161ee5fac5
UniqueDevStorm <me@devstorm.kr>
2021-09-08 21:37:16 +0900

    feat: base client

--- example.py
+++ example.py
@@ -3,8 +3,7 @@ import asyncio
 
 
 async def run():
-    crn = corona.Client(
-        '2rVxc96OKnB%2BFzDfQgVjSNuYgpEHXCnfJ%2B8WRbtKI1R0Gao%2BCUEJNfe1kr5S3OxYR2qbhpkUDdLN0XAmZDbIiA%3D%3D')
+    crn = corona.Client('Your API Key')
     print(await crn.todayCorona())
 
 
